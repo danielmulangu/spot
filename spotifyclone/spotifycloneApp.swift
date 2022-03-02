0a55c5a2eb85b8ccc1edee8f0ea7913c186927e5
danielmulangu <76424137+danielmulangu@users.noreply.github.com>
2022-03-02 14:39:14 +0300

    Initial Commit

---
+++ spotifyclone/spotifycloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotifycloneApp.swift
+//  spotifyclone
+//
+//  Created by Daniel Mulangu on 3/2/22.
+//
+
+import SwiftUI
+
+@main
+struct spotifycloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
